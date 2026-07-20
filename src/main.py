abf86a853c0714b16f0f4e04ea9bd7c73976edd5
ntorresland-dev <ntorresland.dev@gmail.com>
2026-07-20 16:34:13 -0400

    Text output has been modified

--- src/main.py
+++ src/main.py
@@ -1,6 +1,6 @@
 import sys
 from file_management import get_text_content
-from stats import get_words_count
+from stats import get_character_count, get_words_count
 
 
 
@@ -12,8 +12,16 @@ def main():
     path = sys.argv[1]
     text_content = get_text_content(path)
 
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {path}")
+
+    print("----------- Word Count ----------")
     words_counted = get_words_count(text_content)
     print(f"Found {words_counted} total words")
+
+    print("-------- Character Count --------")
+    character_counted = get_character_count(text_content)
+    print(f"Total characters: {character_counted}")
 
 if __name__ == "__main__":
     main()
