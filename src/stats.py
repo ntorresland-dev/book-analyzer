66e8512cc72770b50fcd9fd605eeccaea5b87699
ntorresland-dev <ntorresland.dev@gmail.com>
2026-07-20 16:27:48 -0400

    Add character counting

--- src/stats.py
+++ src/stats.py
@@ -1,2 +1,13 @@
 def get_words_count(text: str) -> int:
     return len(text.split())
+
+def get_character_count(text: str) -> int:
+    total_characters = 0
+
+    for character in text:
+        if character == " " or character == "\n":
+            continue
+
+        total_characters += 1
+
+    return total_characters
