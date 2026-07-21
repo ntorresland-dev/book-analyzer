418998b4c21b091ffd0fab20f5bcfb1bd5314da2
ntorresland-dev <ntorresland.dev@gmail.com>
2026-07-21 00:02:04 -0400

    Create dictionary value sorting function and helper

--- src/text_manipulation.py
+++ src/text_manipulation.py
@@ -5,3 +5,15 @@ def get_letter_frequency(text: str) -> dict[str, int]:
             letter_frequency[letter] = letter_frequency.get(letter, 0) + 1
 
     return letter_frequency
+
+
+def sort_on(letter: tuple[str, int]) -> int:
+    return letter[1]
+
+def dict_to_sorted_list(dict_frequency) -> list[tuple[str, int]]:
+    tuple_list = []
+    for letter in dict_frequency:
+        tuple_list.append((letter, dict_frequency[letter]))
+
+    sorted_list = sorted(tuple_list, key=sort_on, reverse=True)
+    return sorted_list
